913f579e7a8718ea83b6af1fc88d354ffb725a00
Oscar Costero <oscarcostero@gmail.com>
2024-12-10 00:17:36 -0400

    fix on icons

--- app/page.js
+++ app/page.js
@@ -83,17 +83,6 @@ export default function Home() {
     }
   };
 
-  if (isRedirecting) {
-    return (
-      <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 mx-auto"></div>
-          <p className="mt-4 text-gray-600">Abriendo app...</p>
-        </div>
-      </div>
-    );
-  }
-
   if (isLoading) {
     return (
       <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
